53829b360b6384823308b1976c8342530b009543
Anand jha <codewithann@gmail.com>
2023-10-14 21:53:34 +0530

    added logo and Brand name

--- lib/logi_ui.dart
+++ lib/logi_ui.dart
@@ -10,6 +10,47 @@ class ToolBoxLogin extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image(
+                  image: AssetImage('images/logo.png'),
+                  height: 65,
+                  width: 65,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Tool',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'Rubik Medium',
+                        fontSize: 30,
+                      ),
+                    ),
+                    Text(
+                      'Box',
+                      style: TextStyle(
+                        color: Color.fromARGB(244, 249, 113, 59),
+                        fontFamily: 'Rubik Medium',
+                        fontSize: 30,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 40,
+            ),
             Center(
               child: Text(
                 'Log in',
@@ -20,7 +61,20 @@ class ToolBoxLogin extends StatelessWidget {
                 ),
               ),
             ),
-            Center(),
+            SizedBox(
+              height: 20,
+            ),
+            Center(
+              child: Text(
+                textAlign: TextAlign.center,
+                'App without visitors, \nis like a ship lost in the horizon ',
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontFamily: 'Rubik Regular',
+                  fontSize: 14,
+                ),
+              ),
+            ),
           ],
         ),
       ),
